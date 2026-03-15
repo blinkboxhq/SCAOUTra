d5faa362fe6e726f0090d6270b99c67db277d697
BlinkBox <blinkbox.co.in@gmail.com>
2026-03-16 01:02:39 +0530

    Update WEBHOOK_URL in settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   WEBHOOK_URL:
-    'https://script.google.com/macros/s/AKfycbw2_K147T9YjPZqj0MGtkePCr7R2hRVo3uNyxR4YevBGP805fYbHGVPZ8fvepiucrJC/exec',
+    'https://script.google.com/macros/library/d/11v-ojgIKsQDOIXh-mF9cLQR_RZ9lAUWp5QVBEDmh1vk1r7-ybllInIwT/6',
   DEFAULTS: {
     HOURLY_RATE_EUR: 50,
     AVG_MANUAL_HOURS: 15,
