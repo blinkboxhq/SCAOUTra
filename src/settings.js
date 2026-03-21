fbfd29148255311bbd3aec63c1c2ace36745d480
Aaina <ceoaaina1997@gmail.com>
2026-03-22 00:32:27 +0530

    Update webhook URL to new Apps Script deployment

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   WEBHOOK_URL:
-    'https://script.google.com/macros/s/AKfycbwGBpeU9GwvYSEFzR9LlmgLxpZkkMLWTx-CBYWdSPxFk7UwVtHrocflU3_HcbHwuHA/exec',
+"https://script.google.com/macros/s/AKfycby5Y1le9y1ckTFEsgg6P1SXzG3gLle2TIhDZDE19vgCOfnG5wZGBU5aCdL-Mv4ZQOJX/exec"
   DEFAULTS: {
     HOURLY_RATE_EUR: 50,
     AVG_MANUAL_HOURS: 15,
